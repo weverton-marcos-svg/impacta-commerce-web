11cd9922df5ebc13d9584e8c8ddea69d5a5cd601
Weverton Marcos <wevertonmarcos.842@gmail.com.br>
2024-11-03 17:56:56 -0300

    Aula 05 - Criando componentes do carrinho de compra

---
+++ components/ShoppingCart/Summary.js
@@ -0,0 +1,19 @@
+function Summary(props) {
+    const subtotal = props.products
+        .map((x) => x.unitPrice * x.qty)
+        .reduce((prev, e) => prev + e, 0);
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-6"></div>
+          <div className="col-4 text-end">SUBTOTAL</div>
+          <div className="col-2 text-end">
+            <h5>R$ {subtotal}</h5>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Summary;
