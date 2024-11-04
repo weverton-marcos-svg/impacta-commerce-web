f751b8599cf93ddb73bcf9e3fb84ecbd82bb4bdf
Weverton Marcos <wevertonmarcos.842@gmail.com.br>
2024-11-03 21:07:31 -0300

    Aula 06 - Navegação entre paginas

--- pages/products.js
+++ pages/products.js
@@ -1,34 +1,7 @@
 import React, { useState, useEffect } from "react";
-import style from '../styles/Products.module.css'
+import ProductForSalesList from "../components/ProductsForSaleList"
 
-function Installment(props){
-    const fees = props.instainstallment.hasFree ? "com Juros" :"sem juros";
-
-    return(
-        <p>
-            em {props.instainstallment.number}x de R${props.instainstallment.total} {fees} 
-        </p>
-    )
-}
-
-function ProductListItem(props){
-    const defaultProductImage = "https://placehold.co/150";
-
-    return(
-        <div className={style.rowItem}>
-            <img src={defaultProductImage} className="flex-shrink-0 me-3" />
-            <div className={style.itemText}>
-                <a href="#" className="stretched-link">
-                    <h3 className="mt-0">{props.product.title}</h3>
-                </a>
-                <h4>R$ {props.product.amount}</h4>
-                <Installment instainstallment={props.product.installments} />
-            </div>
-        </div>
-    )
-}
-
-export default function ProductForSalesList(){
+export default function ProductForSalesLista(){
    const [error, setError] = useState(null);
    const [isLoaded, setIsLoaded] = useState(false);
    const [products, setProducts] = useState([]);
@@ -55,8 +28,6 @@ export default function ProductForSalesList(){
         return <div>Carregando..</div>
     }
     else{
-        const p = products.map(
-            (x, index) => <ProductListItem product={x} key={index} />);
-            return <div className={style.container}>{p}</div>
+        return <ProductForSalesList product={products} />
     }
 }
